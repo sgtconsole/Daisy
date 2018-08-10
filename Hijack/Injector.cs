e5f97e58d4a7b6eee72dd1febeb5a280ea381cfb
tcook82 <tom@tls.com>
2018-08-10 08:01:04 +0100

    push

--- Hijack/Injector.cs
+++ Hijack/Injector.cs
@@ -8,7 +8,14 @@ namespace Hijack
 {
     public partial class Injector
     {
-        private const string BootstrapperName = @"C:\Git\COB\Hijack\Hijack.Sample\bin\x64\Debug\Bootstrapper.dll";
+        private string BootstrapperName
+        {
+            get
+            {
+                var fi = new FileInfo("bootstrapper.dll");
+                return fi.FullName;
+            }
+        }
 
         public void Inject(int processId)
         {
@@ -30,12 +37,13 @@ namespace Hijack
                 AllocationType.Reserve | AllocationType.Commit,
                 MemoryProtection.ReadWrite);
 
+            IntPtr bytesWritten;
             WriteProcessMemory(
                 targetProcessHandle,
                 bootstrapperNameArgumentAllocation,
                 boostrapperNameData,
                 (int) bootstrapperLength,
-                out _);
+                out bytesWritten);
 
             var loadLibraryDelegate =  GetProcAddress(kernel32Handle, "LoadLibraryA");
 
@@ -48,7 +56,8 @@ namespace Hijack
             );
             
             WaitForSingleObject(loadLibraryThread, 0xFFFFFFFF);
-            GetExitCodeThread(loadLibraryThread, out _);
+            IntPtr exitCode;
+            GetExitCodeThread(loadLibraryThread, out exitCode);
             VirtualFreeEx(targetProcessHandle, bootstrapperNameArgumentAllocation, 0, AllocationType.Release);
 
             var proc = Process.GetProcessById(processId);
@@ -67,14 +76,35 @@ namespace Hijack
 
         public void Invoke()
         {
-            var handle = LoadLibraryExA(BootstrapperName, IntPtr.Zero, LoadLibraryFlags.DontResolveDllReferences);
-            var dosHeader = Marshal.PtrToStructure<IMAGE_DOS_HEADER>(handle);
-            var ntHeader = Marshal.PtrToStructure<IMAGE_NT_HEADERS32>(IntPtr.Add(handle, dosHeader.e_lfanew));
-            var exportDirectory = Marshal.PtrToStructure<IMAGE_EXPORT_DIRECTORY>(IntPtr.Add(handle, (int) ntHeader.OptionalHeader.ExportTable.VirtualAddress));
-            
+            const string bootstrapperEntryPoint = "LoadManagedProject";
+
+            unsafe
+            {
+                var handle = LoadLibraryExA(BootstrapperName, IntPtr.Zero, LoadLibraryFlags.DontResolveDllReferences);
+                var dosHeader = Marshal.PtrToStructure<IMAGE_DOS_HEADER>(handle);
+                var ntHeader = Marshal.PtrToStructure<IMAGE_NT_HEADERS32>(IntPtr.Add(handle, dosHeader.e_lfanew));
+                var exportDirectory = Marshal.PtrToStructure<IMAGE_EXPORT_DIRECTORY>(IntPtr.Add(handle, (int)ntHeader.OptionalHeader.ExportTable.VirtualAddress));
+
+                var nameRef = (uint*)new IntPtr(handle.ToInt32() + exportDirectory.AddressOfNames);
+                var ordinal = (ushort*)new IntPtr(handle.ToInt32() + exportDirectory.AddressOfNameOrdinals);
+
+                for (var i = 0; i < exportDirectory.NumberOfNames; i++, nameRef++, ordinal++)
+                {
+                    var str = new IntPtr(handle.ToInt32() + (int)(*nameRef));
+                    var functionName = Marshal.PtrToStringAnsi(str);
+                    if (functionName == bootstrapperEntryPoint)
+                    {
+                        var tmpaa = (UInt32*)(handle.ToInt32() + (exportDirectory.AddressOfFunctions + (*ordinal * 4)));
+                        var addr = (UInt32)((handle.ToInt32()) + (*tmpaa));
 
 
-            var error = Marshal.GetLastWin32Error();
+
+                    }
+                }
+                
+            }
+
+          
         }
 
 
