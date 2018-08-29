49148f362e9413f7bb0431e1ad14b5037ca1318f
Tom <tom@tomcook.info>
2018-08-29 17:05:19 +0100

    Added Launcher

---
+++ Daisy.Launcher/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Daisy.Launcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var notepadProcess = Process.GetProcessesByName("notepad++").FirstOrDefault();
+            notepadProcess.Invoke(typeof(Sample.Program),"EntryPoint");
+            Console.ReadLine();
+        }
+    }
+}
